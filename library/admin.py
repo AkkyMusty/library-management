d260ee7a264fd0707aba65678ac953638b925c5c
AkkyMusty <153207112+AkkyMusty@users.noreply.github.com>
2025-11-24 23:22:07 +0100

    Registered the book models into admin, created the home page to view all books

--- library/admin.py
+++ library/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
 
+from .models import Book, BookCopy, BorrowRecord
+
+
 # Register your models here.
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'isbn')
+    search_fields = ('title', 'author', 'isbn')
+
+@admin.register(BookCopy)
+class BookCopyAdmin(admin.ModelAdmin):
+    list_display = ('book', 'barcode', 'status')
+    list_filter = ('status', 'book')
+    search_fields = ('barcode',)
+
+@admin.register(BorrowRecord)
+class BorrowRecordAdmin(admin.ModelAdmin):
+    list_display = ('copy', 'user', 'borrow_date', 'due_date', 'return_date')
+    list_filter = ('borrow_date', 'return_date')
+    search_fields = ('user__username', 'copy__barcode')
+
+
