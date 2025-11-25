0bbb2753db3dfa442cc5879844ee5572fc3209b7
AkkyMusty <153207112+AkkyMusty@users.noreply.github.com>
2025-11-25 23:29:05 +0100

    Added book details view to show book details and availability

--- library/urls.py
+++ library/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.book_list, name='home'),
     path('books/', views.book_list, name='book_list'),
+    path('books/<int:book_id>/', views.book_detail, name='book_detail'),
+
 ]
--- library/views.py
+++ library/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Book
 # Create your views here.
 
@@ -7,3 +7,12 @@ def book_list(request):
     books = Book.objects.all()
     return render(request, 'library/book_list.html', {'books': books})
 
+
+def book_detail(request, book_id):
+    book = get_object_or_404(Book, id=book_id)
+    copies = book.copies.all()  # because we used related_name='copies'
+
+    return render(request, 'library/book_detail.html', {
+        'book': book,
+        'copies': copies
+    })
