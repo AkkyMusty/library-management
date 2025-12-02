3b9894baeea00ba64ed25052f378eef1d0e4ef3f
AkkyMusty <153207112+AkkyMusty@users.noreply.github.com>
2025-12-02 23:38:38 +0100

    Added a simple book search feature

--- library/views.py
+++ library/views.py
@@ -10,10 +10,27 @@ from .models import Book, BookCopy, BorrowRecord
 # Create your views here.
 
 
-def book_list(request):
-    books = Book.objects.all()
-    return render(request, 'library/book_list.html', {'books': books})
+# def book_list(request):
+#     books = Book.objects.all()
+#     return render(request, 'library/book_list.html', {'books': books})
 
+
+def book_list(request):
+    query = request.GET.get('q', '')
+
+    if query:
+        books = Book.objects.filter(
+            Q(title__icontains=query) |
+            Q(description__icontains=query) |
+            Q(title__icontains=query)
+        )
+    else:
+        books = Book.objects.all()
+
+    return render(request, 'library/book_list.html', {
+        'books': books,
+        'query': query,
+    })
 
 def book_detail(request, book_id):
     book = get_object_or_404(Book, id=book_id)
